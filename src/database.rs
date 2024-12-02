7a755845566513eade879e90c6614743488d7a91
Giovanni Karra <karragiovanni@gmail.com>
2024-12-02 17:22:35 +0100

    functions to access the database

--- src/database.rs
+++ src/database.rs
@@ -3,6 +3,30 @@ use std::path::PathBuf;
 use std::fs::File;
 use sqlx::SqlitePool;
 
+use libmatcompl::FMatrix;
+
+
+#[derive(Debug)]
+struct Student {
+	id: i64,
+	name: String
+}
+
+#[derive(Debug)]
+struct Course {
+	id: i64,
+	code: String,
+	name: String
+}
+
+#[derive(Debug)]
+pub struct Rating {
+	id: i64,
+	student: i64,
+	course: i64,
+	rating: i64
+}
+
 pub async fn reset_db(path: PathBuf, schema: String) -> Result<SqlitePool, String> {
 	let _file = File::create(&path).map_err(|e| e.to_string())?;
 	let db_url = "sqlite://".to_owned() + path
@@ -19,4 +43,161 @@ pub async fn reset_db(path: PathBuf, schema: String) -> Result<SqlitePool, Strin
 		.map_err(|e| e.to_string())?;
 
 	Ok(pool)
+}
+
+
+pub async fn add_student(name: String, pool: &SqlitePool) -> Result<(), String> {
+	let student = sqlx::query_as!(
+		Student,
+		"
+		SELECT id, name FROM students
+		WHERE name = ?;
+		",
+		name
+	)
+	.fetch_optional(pool)
+	.await
+	.map_err(|e| e.to_string())?;
+
+	if student.is_some() { return Err("Student with this name already exists".to_owned()) }
+	
+	sqlx::query!(
+		"
+		INSERT INTO students (name)
+		VALUES (?);
+		",
+		name
+	)
+	.execute(pool)
+	.await
+	.map_err(|e| e.to_string())?;
+	
+	Ok(())
+}
+
+
+pub async fn add_course(name: String, code: String, pool: &SqlitePool) -> Result<(), String> {
+	let course = sqlx::query_as!(
+		Course,
+		"
+		SELECT id, code, name FROM courses
+		WHERE name = ? AND code = ?;
+		",
+		name, code
+	)
+	.fetch_optional(pool)
+	.await
+	.map_err(|e| e.to_string())?;
+
+	if course.is_some() { return Err("Course already exists".to_owned()) }
+	
+	sqlx::query!(
+		"
+		INSERT INTO courses (code, name)
+		VALUES (?, ?);
+		",
+		code, name
+	)
+	.execute(pool)
+	.await
+	.map_err(|e| e.to_string())?;
+	
+	Ok(())
+}
+
+
+pub async fn add_rating(student_id: i64, course_id: i64, rating: i64, pool: &SqlitePool) -> Result<(), String> {
+	sqlx::query!(
+		"
+		INSERT INTO ratings (student, course, rating)
+		VALUES (?, ?, ?);
+		",
+		student_id, course_id, rating
+	)
+	.execute(pool)
+	.await
+	.map_err(|e| e.to_string())?;
+	
+	Ok(())
+}
+
+
+pub async fn get_student_id(name: String, pool: &SqlitePool) -> Result<i64, String> {
+	sqlx::query_as!(
+		Student,
+		"
+		SELECT id, name FROM students
+		WHERE name = ?;
+		",
+		name
+	)
+	.fetch_optional(pool)
+	.await
+	.map_err(|e| e.to_string())?
+	.map(|s| s.id)
+	.ok_or("Couldn't fetch student id, probably incorrect name".to_owned())
+}
+
+
+pub async fn get_student_ratings(student_id: i64, pool: &SqlitePool) -> Result<Vec<Rating>, String> {
+	sqlx::query_as!(
+		Rating,
+		"
+		SELECT id, student, course, rating FROM ratings
+		WHERE student = ?;
+		",
+		student_id
+	)
+	.fetch_all(pool)
+	.await
+	.map_err(|e| e.to_string())
+}
+
+
+pub async fn get_ratings_matrix(pool: &SqlitePool) -> Result<FMatrix, String> {
+	let m = get_student_count(pool).await?;
+	let n = get_course_count(pool).await?;
+
+	let data = sqlx::query_scalar!(
+		"
+		SELECT rating
+		FROM ratings
+		ORDER BY student, course
+		"
+	)
+	.fetch_all(pool)
+	.await
+	.map_err(|e| e.to_string())?
+	.iter()
+	.map(|elem| *elem as f32)
+	.collect();
+
+
+	Ok(FMatrix::new(m, n, data))
+}
+
+
+pub async fn get_student_count(pool: &SqlitePool) -> Result<usize, String> {
+	sqlx::query_scalar!(
+		"
+		SELECT COUNT(*) FROM students;
+		"
+	)
+	.fetch_one(pool)
+	.await
+	.map_err(|e| e.to_string())
+	.map(|res| res as usize)
+}
+
+
+pub async fn get_course_count(pool: &SqlitePool) -> Result<usize, String> {
+	sqlx::query_scalar!(
+		"
+		SELECT COUNT(*) FROM courses;
+		"
+	)
+	.fetch_one(pool)
+	.await
+	.map_err(|e| e.to_string())
+	.map(|res| res as usize)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use database::reset_db;
 use rocket::{fs::{relative, NamedFile}, launch};
-use std::{path::{Path, PathBuf}, str::FromStr};
+use std::{env, path::{Path, PathBuf}, str::FromStr};
 use sqlx::SqlitePool;
 
 mod database;
@@ -25,15 +25,24 @@ pub async fn serve_assets(path: PathBuf) -> Option<NamedFile> {
 
 #[launch]
 async fn rocket() -> _ {
-
+	let args: Vec<String> = env::args().collect();
+	dotenvy::dotenv().ok();
+	let database_url = env::var("DATABASE_URL")
+		.expect("Expected DATABASE_URL in the environment"); 
+	let db_path = PathBuf::from_str("data.db").unwrap();
+	
 	let schema = include_str!("../database/schema.sql").to_owned();
-	let db_pool = SqlitePool::connect("sqlite://data.db")
-		.await
-		.unwrap_or(
-			reset_db(PathBuf::from_str("data.db").unwrap(), schema)
+	let db_pool = match args.contains(&"--reset-db".to_owned()) {
+		true => reset_db(db_path, schema)
+			.await
+			.expect("DB creation error"),
+		false => match SqlitePool::connect(&database_url).await {
+			Ok(connection) => connection,
+			Err(_) => reset_db(db_path, schema)
 				.await
 				.expect("DB creation error")
-		);
+		}
+	};
 
 	let rocket = rocket::build()
 		.mount("/", rocket::routes![serve, serve_assets])
